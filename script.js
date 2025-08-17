bfb8046a69bb6ee84a682d63522a15fe2e08e6e6
Sheikh Hassaan Saeed <sheikhhassaansaeed@gmail.com>
2025-08-17 16:54:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () =>{
+
     const now = new Date();
     const dateTimeUpdate = document.getElementById('date-time');
     if(dateTimeUpdate){
         dateTimeUpdate.textContent = now.toLocaleString();
     }
     alert("Welcome to my mini project");
+
 });
